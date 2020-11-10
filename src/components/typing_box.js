f01c9d0b62e4a5df4bcda9c4c0adff976e889819
Danny Jeng <jeng.danny@gmail.com>
2020-11-09 23:28:43 -0500

    Added basic countdown timer as a button for now.

--- src/components/typing_box.js
+++ src/components/typing_box.js
@@ -3,6 +3,9 @@ import Container from 'react-bootstrap/Container';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Card from 'react-bootstrap/Card';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
 
 import './typing_box.css';
 
@@ -11,6 +14,50 @@ class TypingBox extends Component {
         super(props)
         this.state = {
             word: '',
+            time: {},
+        };
+        this.timer = 0;
+        this.seconds = 60; // The starting time on the clock.
+    }
+
+    componentDidMount() {
+        let timeLeft = this.secondsToTime(this.seconds);
+        this.setState({ time: timeLeft });
+    }
+
+    secondsToTime(s) {
+        let divisor_for_minutes = s % (60*60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
+
+        let divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+
+        if (seconds < 10) {
+            seconds = '0' + seconds; // For padding the seconds.
+        }
+
+        let obj = {
+            'm': minutes,
+            's': seconds,
+        };
+
+        return obj;
+    }
+
+    startTimer = () => {
+        if (this.timer === 0 && this.seconds > 0) {
+            this.timer = setInterval(this.countDown, 1000);
+        }
+    }
+
+    countDown = () => {
+        this.seconds = this.seconds - 1;
+        this.setState({
+            time: this.secondsToTime(this.seconds),
+        });
+
+        if (this.seconds === 0) {
+            clearInterval(this.timer); // To stop setInterval() from going into the negatives.
         }
     }
 
@@ -37,7 +84,7 @@ class TypingBox extends Component {
     render() {
         return (
             <Container>
-                <h1>Typing Test</h1>
+                <Button onClick={this.startTimer}>{this.state.time.m}:{this.state.time.s}</Button>
                 <Card>
                     <Card.Body>
                         <Card.Text>
@@ -45,6 +92,12 @@ class TypingBox extends Component {
                         </Card.Text>
                     </Card.Body>
                 </Card>
+                
+                {/**
+                    Put a refresh symbol next to the input text box.
+                    Start timer countdown when this.state.word is not empty.
+                **/}
+
                 <InputGroup onKeyUp={this.handleKeyUp}>
                     <FormControl 
                     type='text'
